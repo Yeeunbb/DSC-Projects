451f6a02e252ded8f0540a53ae37fd1cc4e7747d
yeeun <ykoou@naver.com>
2019-10-07 18:16:22 +0900

    My First App

--- main.dart
+++ main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
 import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
@@ -11,90 +10,18 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Startup Name Generator',
       home: RandomWords(),
-=======
-
-class Product {
-  const Product({this.name});
-  final String name;
-}
-
-typedef void CartChangedCallback(Product product, bool inCart);
-
-class ShoppingListItem extends StatelessWidget {
-  ShoppingListItem({Product product, this.inCart, this.onCartChanged})
-      : product = product,
-        super(key: ObjectKey(product));
-
-  final Product product;
-  final bool inCart;
-  final CartChangedCallback onCartChanged;
-
-  Color _getColor(BuildContext context) {
-    // The theme depends on the BuildContext because different parts of the tree
-    // can have different themes.  The BuildContext indicates where the build is
-    // taking place and therefore which theme to use.
-
-    return inCart ? Colors.black54 : Theme.of(context).primaryColor;
-  }
-
-  TextStyle _getTextStyle(BuildContext context) {
-    if (!inCart) return null;
-
-    return TextStyle(
-      color: Colors.black54,
-      decoration: TextDecoration.lineThrough,
-    );
-  } //I'm yeeun abc
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      onTap: () {
-        onCartChanged(product, inCart);
-      },
-      leading: CircleAvatar(
-        backgroundColor: _getColor(context),
-        child: Text(product.name[0]),
-      ),
-      title: Text(product.name, style: _getTextStyle(context)),
->>>>>>> 45ebcea567344939ec0afe69f8a46c97a4ca250b
     );
   }
 }
 
-<<<<<<< HEAD
 class RandomWordsState extends State<RandomWords> {
   //TODO Add build()) method
   final _suggestions = <WordPair>[];
   final _biggerFont = const TextStyle(fontSize: 18.0);
-=======
-class ShoppingList extends StatefulWidget{
-  ShoppingList({Key key, this.products}) : super(key: key);
-
-  final List<Product> products;
-
-  @override
-  _ShoppingListState createState() => _ShoppingListState();
-}
-
-class _ShoppingListState extends State<ShoppingList> {
-  Set<Product> _shoppingCart = Set<Product>();
-
-  void _handleCartChanged(Product product, bool inCart) {
-    setState(() {
-      if(!inCart)
-        _shoppingCart.add(product);
-      else
-        _shoppingCart.remove(product);
-    });
-  }
-
->>>>>>> 45ebcea567344939ec0afe69f8a46c97a4ca250b
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-<<<<<<< HEAD
         title: Text('Startup Name Generator'),
       ),
       body: _buildSuggestions(),
@@ -103,16 +30,16 @@ class _ShoppingListState extends State<ShoppingList> {
 
   Widget _buildSuggestions(){
     return ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: /*1*/ (context, i) {
-        if(i.isOdd) return Divider(); /*2*/
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: /*1*/ (context, i) {
+          if(i.isOdd) return Divider(); /*2*/
 
-        final index = i ~/ 2; /*3*/
-        if(index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10)); /*4*/
-        }
-        return _buildRow(_suggestions[index]);
-      });
+          final index = i ~/ 2; /*3*/
+          if(index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+          }
+          return _buildRow(_suggestions[index]);
+        });
   }
 
   Widget _buildRow(WordPair pair) {
@@ -120,41 +47,12 @@ class _ShoppingListState extends State<ShoppingList> {
       title: Text(
         pair.asPascalCase,
         style: _biggerFont,
-=======
-        title: Text('Yeeun Shopping List!'),
-      ),
-      body: ListView(
-        padding: EdgeInsets.symmetric(vertical: 8.0),
-        children: widget.products.map((Product product) {
-          return ShoppingListItem(
-            product: product,
-            inCart: _shoppingCart.contains(product),
-            onCartChanged: _handleCartChanged,
-          );
-        }).toList(),
->>>>>>> 45ebcea567344939ec0afe69f8a46c97a4ca250b
       ),
     );
   }
 }
 
-<<<<<<< HEAD
 class RandomWords extends StatefulWidget{
   @override
   RandomWordsState createState() => RandomWordsState();
 }
-
-=======
-void main() {
-  runApp(MaterialApp(
-    title: 'Shopping App',
-    home: ShoppingList(
-      products: <Product>[
-        Product(name: 'Eggs'),
-        Product(name: 'Flour'),
-        Product(name: 'Chocolate chips'),
-      ],
-    ),
-  ));
-}
->>>>>>> 45ebcea567344939ec0afe69f8a46c97a4ca250b
