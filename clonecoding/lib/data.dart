6a80016e06d32d6ec4c9190fb7b5fa851be8fac2
yeeun <ykoou@naver.com>
2020-02-01 22:44:56 +0900

    clond coding

---
+++ clonecoding/lib/data.dart
@@ -0,0 +1,13 @@
+List<String> images = [
+  "assets/choco_04.jpg",
+  "assets/choco_03.jpg",
+  "assets/choco_02.jpg",
+  "assets/choco_01.jpg",
+];
+
+List<String> title = [
+  "Lovely Sleeping Choco",
+  "Sleepy Choco",
+  "After shower Choco",
+  "Big Eyes Cute Choco",
+];
